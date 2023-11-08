5c74a82d4ebcd7a7436681d0cca47519dcea458b
Ye Naing Tun <yenaingtun425@gmail.com>
2023-11-08 09:49:16 +0630

    added start and restart button

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
 const tics = document.querySelectorAll('.tic');
 const button_x = document.querySelector('.button_x');
 const button_o = document.querySelector('.button_o');
+const button_start = document.querySelector('.button_start');
+const button_restart = document.querySelector('.button_restart');
 let x = [];
 let o = [];
 let user_1;
 let user_2;
 let turn;
+let result;
 
+button_start.addEventListener('click', () => {
+    document.querySelector('.gameboard').classList.remove('hide');
+    button_start.classList.add('hide');
+});
 button_x.addEventListener('click', () => {
     user_1 = 'X';
     user_2 = 'O';
@@ -23,44 +30,46 @@ button_o.addEventListener('click', () => {
 // get the data from click
 tics.forEach(tic => {
     tic.addEventListener('click', () => {
-        if (user_1 === 'X') {
-            if (turn === 'X') {
-                if (!x.includes(tic.value) && !o.includes(tic.value)) {
-                    x.push(tic.value);
-                    tic.classList.add('playerX');
-                    turn = 'O';
-                    game_logic();
+        if (!result) {
+            if (user_1 === 'X') {
+                if (turn === 'X') {
+                    if (!x.includes(tic.value) && !o.includes(tic.value)) {
+                        x.push(tic.value);
+                        tic.classList.add('playerX');
+                        turn = 'O';
+                        game_logic();
+                    }
+                } else {
+                    if (!x.includes(tic.value) && !o.includes(tic.value)) {
+                        o.push(tic.value);
+                        tic.classList.add('playerO');
+                        turn = 'X';
+                        game_logic();
+                    }
                 }
+                console.log('x ' + x);
+                console.log('o ' + o);
+            } else if (user_1 === 'O') {
+                if (turn === 'X') {
+                    if (!x.includes(tic.value) && !o.includes(tic.value)) {
+                        x.push(tic.value);
+                        tic.classList.add('playerX');
+                        turn = 'O';
+                        game_logic();
+                    }
+                } else {
+                    if (!x.includes(tic.value) && !o.includes(tic.value)) {
+                        o.push(tic.value);
+                        tic.classList.add('playerO');
+                        turn = 'X';
+                        game_logic();
+                    }
+                }
+                console.log('x ' + x);
+                console.log('o ' + o);
             } else {
-                if (!x.includes(tic.value) && !o.includes(tic.value)) {
-                    o.push(tic.value);
-                    tic.classList.add('playerO');
-                    turn = 'X';
-                    game_logic();
-                }
+                alert('need to select X or O');
             }
-            console.log('x ' + x);
-            console.log('o ' + o);
-        } else if (user_1 === 'O') {
-            if (turn === 'X') {
-                if (!x.includes(tic.value) && !o.includes(tic.value)) {
-                    x.push(tic.value);
-                    tic.classList.add('playerX');
-                    turn = 'O';
-                    game_logic();
-                }
-            } else {
-                if (!x.includes(tic.value) && !o.includes(tic.value)) {
-                    o.push(tic.value);
-                    tic.classList.add('playerO');
-                    turn = 'X';
-                    game_logic();
-                }
-            }
-            console.log('x ' + x);
-            console.log('o ' + o);
-        } else {
-            alert('need to select X or O');
         }
     });
 });
@@ -103,6 +112,18 @@ tics.forEach(tic => {
 
 // array value check
 
+button_restart.addEventListener('click', () => {
+    button_restart.classList.add('hide');
+    button_x.classList.remove('hide');
+    button_o.classList.remove('hide');
+    reset();
+});
+
+function restart() {
+    button_x.classList.add('hide');
+    button_o.classList.add('hide');
+}
+
 function arrayHasValues(arr, values) {
     return values.every(value => arr.includes(value));
 }
@@ -116,7 +137,8 @@ function reset() {
     tics.forEach(tic => {
         tic.classList.remove('playerX');
         tic.classList.remove('playerO');
-    })
+    });
+    result = '';
 }
 
 // possible wining logic
@@ -130,13 +152,22 @@ function game_logic() {
     const possible_7 = ['1', '5', '9'];
     const possible_8 = ['3', '5', '7'];
     if (arrayHasValues(x, possible_1) || arrayHasValues(x, possible_2) || arrayHasValues(x, possible_3) || arrayHasValues(x, possible_4) || arrayHasValues(x, possible_5) || arrayHasValues(x, possible_6) || arrayHasValues(x, possible_7) || arrayHasValues(x, possible_8)) {
-        alert('X win')
-        reset();
+        result = 'X';
+        button_restart.classList.remove('hide');
+        button_x.classList.add('hide');
+        button_o.classList.add('hide');
+        alert('X win');
     } else if (arrayHasValues(o, possible_1) || arrayHasValues(o, possible_2) || arrayHasValues(o, possible_3) || arrayHasValues(o, possible_4) || arrayHasValues(o, possible_5) || arrayHasValues(o, possible_6) || arrayHasValues(o, possible_7) || arrayHasValues(o, possible_8)) {
-        alert('O win')
-        reset();
+        result = 'O';
+        button_restart.classList.remove('hide');
+        button_x.classList.add('hide');
+        button_o.classList.add('hide');
+        alert('O win');
     } else {
-        console.log('draw');
-        // reset();
+        button_restart.classList.remove('hide');
+        button_x.classList.add('hide');
+        button_o.classList.add('hide');
+        // result = 'D';
+        // alert('Draw');
     }
 };
